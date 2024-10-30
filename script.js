d7d7345df2bcca8fdf687bde3faf947949723259
Revanth1584 <rkrevanth295@gmail.com>
2024-10-30 21:07:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-// "Hello" Text Animation in Different Languages
+// Array of "Hello" in different languages
 const hellos = ["Hello", "नमस्ते", "హలో", "Bonjour", "Hola"];
 let helloIndex = 0;
 const helloElement = document.getElementById("helloText");
 
-function changeHello() {
+function showHello() {
   helloElement.innerText = hellos[helloIndex];
+  helloElement.style.opacity = 1;
+
+  // Cycle through the hellos array
   helloIndex = (helloIndex + 1) % hellos.length;
+
+  // Hide and update the hello text
+  setTimeout(() => {
+    helloElement.style.opacity = 0;
+  }, 3000);
 }
 
-// Change "Hello" text every 3 seconds
-setInterval(changeHello, 3000);
-changeHello();
+// Show each "Hello" on load
+setInterval(showHello, 3500);
+showHello();
